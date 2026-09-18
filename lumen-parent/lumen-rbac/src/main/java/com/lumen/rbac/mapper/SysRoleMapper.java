37ed3427588f58b76018a256751c19bdedaa6808
fang <fang@chaonao.com>
2026-09-18 12:33:20 +0800

    feat(rbac): 添加错误码 + 7 个 Mapper

---
+++ lumen-parent/lumen-rbac/src/main/java/com/lumen/rbac/mapper/SysRoleMapper.java
@@ -0,0 +1,9 @@
+package com.lumen.rbac.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lumen.rbac.entity.SysRole;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SysRoleMapper extends BaseMapper<SysRole> {
+}
